54acf22ff6d075ea13be2df68e2b91a73203f390
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2017-07-07 14:28:49 +0530

    Enable Data binding and Add Libraries

--- app/src/main/java/com/mindorks/framework/mvvm/MainActivity.java
+++ app/src/main/java/com/mindorks/framework/mvvm/MainActivity.java
@@ -16,8 +16,8 @@
 
 package com.mindorks.framework.mvvm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
