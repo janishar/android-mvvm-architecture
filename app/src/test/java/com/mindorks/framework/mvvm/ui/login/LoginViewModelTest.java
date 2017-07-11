f21f2ef4fea58853e920ecf412eda4f5ae89eed2
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2017-07-11 19:18:12 +0530

    Add test for login viewModel

---
+++ app/src/test/java/com/mindorks/framework/mvvm/ui/login/LoginViewModelTest.java
@@ -0,0 +1,97 @@
+/*
+ *  Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://mindorks.com/license/apache-v2
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package com.mindorks.framework.mvvm.ui.login;
+
+import com.mindorks.framework.mvvm.data.DataManager;
+import com.mindorks.framework.mvvm.data.model.api.LoginRequest;
+import com.mindorks.framework.mvvm.data.model.api.LoginResponse;
+import com.mindorks.framework.mvvm.utils.rx.TestSchedulerProvider;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.TestScheduler;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by amitshekhar on 11/07/17.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoginViewModelTest {
+
+    @Mock
+    LoginCallback mLoginCallback;
+    @Mock
+    DataManager mMockDataManager;
+
+    private LoginViewModel mLoginViewModel;
+    private TestScheduler mTestScheduler;
+
+    @BeforeClass
+    public static void onlyOnce() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        CompositeDisposable compositeDisposable = new CompositeDisposable();
+        mTestScheduler = new TestScheduler();
+        TestSchedulerProvider testSchedulerProvider = new TestSchedulerProvider(mTestScheduler);
+        mLoginViewModel = new LoginViewModel(
+                mMockDataManager,
+                testSchedulerProvider,
+                compositeDisposable);
+        mLoginViewModel.setCallback(mLoginCallback);
+    }
+
+    @Test
+    public void testServerLoginSuccess() {
+
+        String email = "dummy@gmail.com";
+        String password = "password";
+
+        LoginResponse loginResponse = new LoginResponse();
+
+        doReturn(Observable.just(loginResponse))
+                .when(mMockDataManager)
+                .doServerLoginApiCall(new LoginRequest
+                        .ServerLoginRequest(email, password));
+
+        mLoginViewModel.login(email, password);
+
+        mTestScheduler.triggerActions();
+
+        verify(mLoginCallback).showLoading();
+        verify(mLoginCallback).hideLoading();
+        verify(mLoginCallback).openMainActivity();
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        mLoginViewModel.onDestroy();
+    }
+
+}
---
+++ app/src/test/java/com/mindorks/framework/mvvm/utils/rx/TestSchedulerProvider.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://mindorks.com/license/apache-v2
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package com.mindorks.framework.mvvm.utils.rx;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.TestScheduler;
+
+/**
+ * Created by amitshekhar on 11/07/17.
+ */
+
+public class TestSchedulerProvider implements SchedulerProvider {
+
+    private final TestScheduler mTestScheduler;
+
+    public TestSchedulerProvider(TestScheduler testScheduler) {
+        this.mTestScheduler = testScheduler;
+    }
+
+    @Override
+    public Scheduler ui() {
+        return mTestScheduler;
+    }
+
+    @Override
+    public Scheduler computation() {
+        return mTestScheduler;
+    }
+
+    @Override
+    public Scheduler io() {
+        return mTestScheduler;
+    }
+
+}
