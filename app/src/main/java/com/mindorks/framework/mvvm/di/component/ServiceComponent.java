728a5b798dc571e3a9926ffe767035455239830d
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2017-07-11 08:27:56 +0530

    Add Service demo with component and module

---
+++ app/src/main/java/com/mindorks/framework/mvvm/di/component/ServiceComponent.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://mindorks.com/license/apache-v2
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package com.mindorks.framework.mvvm.di.component;
+
+import com.mindorks.framework.mvvm.di.PerService;
+import com.mindorks.framework.mvvm.di.module.ServiceModule;
+import com.mindorks.framework.mvvm.service.SyncService;
+
+import dagger.Component;
+
+/**
+ * Created by amitshekhar on 11/07/17.
+ */
+
+@PerService
+@Component(dependencies = ApplicationComponent.class, modules = ServiceModule.class)
+public interface ServiceComponent {
+
+    void inject(SyncService service);
+
+}
